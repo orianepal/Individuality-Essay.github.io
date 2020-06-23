9910c4eeb23b9ebeae1a0e1767192d483029c0ee
orianepal <47210841+orianepal@users.noreply.github.com>
2020-06-23 22:51:41 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,77 @@
+
+
+$(document).ready(function(){
+    $('.img').hover(function(){
+        $('.img').removeClass('hover');
+        $(this).addClass('hover');
+    })
+})
+
+
+  $( function() {
+    $( "#draggable000" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable0" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable00" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable01" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable02" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable03" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable04" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable05" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable1" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable2" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable3" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable33" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable4" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable5" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable6" ).draggable();
+  } );
+
+  $( function() {
+    $( "#draggable7" ).draggable();
+  } );
+   
+
+
+
